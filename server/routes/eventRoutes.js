60b5deaab33bdd936b833e3a613cf71cc25ac63e
Ziyad Hammad <ziyadhammad0@gmail.com>
2023-01-02 23:15:40 -0500

    Starting authentication for eventControllers

--- server/routes/eventRoutes.js
+++ server/routes/eventRoutes.js
@@ -6,8 +6,9 @@ const {
   updateEvent,
   deleteEvent,
 } = require("../controllers/eventController");
+const {protect} = require('../middleware/authMiddleware')
 
-router.route('/').get(getEvents).post(postEvent);
-router.route('/:id').put(updateEvent).delete(deleteEvent);
+router.route('/').get(protect, getEvents).post(protect,postEvent);
+router.route('/:id').put(protect,updateEvent).delete(protect,deleteEvent);
 
 module.exports = router;
