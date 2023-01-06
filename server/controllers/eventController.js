b2673977e3bd17051b3cf4968730bda899e35f6f
Ziyad Hammad <ziyadhammad0@gmail.com>
2023-01-05 19:03:32 -0500

    Completed CRUD & Authentication/Authorization for events & users

--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -1,9 +1,11 @@
 const { response } = require("express");
 const asyncHandler = require("express-async-handler");
 const Event = require("../models/eventSchema");
+const User = require("../models/userSchema");
+
 
 const getEvents = asyncHandler(async (req, res) => {
-  const events = await Event.find();
+  const events = await Event.find({ user: req.user.id});
 
   res.status(200).json(events);
 });
@@ -13,6 +15,7 @@ const postEvent = asyncHandler(async (req, res) => {
     title: req.body.title,
     start: req.body.start,
     end: req.body.end,
+    user_id: req.user.id
   });
 
   res.status(200).json(event);
@@ -20,9 +23,22 @@ const postEvent = asyncHandler(async (req, res) => {
 
 const updateEvent = asyncHandler(async (req, res) => {
   const event = await Event.findById(req.params.id);
+
   if (!event) {
     res.status(400);
     throw new Error(`Cannot find event ${req.params.id}`);
+  }
+
+  const user = await User.findById(req.user.id)
+  // check for user
+  if (!user) {
+    res.status(401)
+    throw new Error(`Cannot find user ${req.user.id}`);
+  }
+  // make sure logged in user matches goal user
+  if (goal.user.toString() !== user.id) {
+    res.status(401)
+    throw new Error(`User not authorized`)
   }
   const updatedEvent = await Event.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
@@ -36,8 +52,19 @@ const deleteEvent = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error(`Cannot find event ${req.params.id}`);
   }
+  const user = await User.findById(req.user.id)
+  // check for user
+  if (!user) {
+    res.status(401)
+    throw new Error(`Cannot find user ${req.user.id}`);
+  }
+  // make sure logged in user matches goal user
+  if (event.user.toString() !== user.id) {
+    res.status(401)
+    throw new Error(`User not authorized`)
+  }
   await event.remove();
-  res.status(200).json({ id: req.params.id });
+  res.status(200).json({ message: `Event ${req.params.id} has been deleted` });
 });
 
 module.exports = {
